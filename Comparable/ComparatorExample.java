42d4232572c7130ec4d1f51dd866ba1279b1cab9
Raj Kumar Pandit <77954257+Rajkumarpandit436@users.noreply.github.com>
2021-09-20 15:55:23 +0530

    Update ComparatorExample.java

--- Comparable/ComparatorExample.java
+++ Comparable/ComparatorExample.java
@@ -25,9 +25,9 @@ class AgeComparator implements Comparator<Student> {
         if (s1.age == s2.age) {
             return 0;
         } else if (s1.age > s2.age) {
-            return -1;
-        } else {
             return 1;
+        } else {
+            return -1;
         }
     }
 }
