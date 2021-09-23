5192b38b970c75fa55118623fb37e0cf2a0df795
Rajkumarpandit436 <rajkumarpandit436@gmail.com>
2021-09-23 17:08:32 +0530

    student task second commit

--- raj-training/src/main/java/com/practice/rajtraining/StudentMain.java
+++ raj-training/src/main/java/com/practice/rajtraining/StudentMain.java
@@ -17,7 +17,7 @@ public class StudentMain {
         m1.setMobile("8298241734");
         m1.setSubject("MTH");
         m1.setMarks(75);
-        
+
         list.add(m1);
         list.add(m1);
         list.add(m1);
@@ -28,37 +28,46 @@ public class StudentMain {
         list.add(m1);
         list.add(m1);
         list.add(m1);
-        
+
         int stud_id;
         String stud_name;
         String mobile;
         String subject;
         int marks;
+
         
-        StudentDetails sd=new StudentDetails();
-        Marks mark=new Marks();
-        ClassDetails cd=new ClassDetails();
-        Student s=new Student();
-        
-        for(Mixture m:list){
-          stud_id=m.getStud_id();
-          sd.setStud_id(stud_id);
-          mark.setStud_id(stud_id);
-          cd.setStud_id(stud_id);
-          
-          stud_name = m.getStud_name();
-          s.setName(stud_name);
-          
-          mobile = m.getMobile();
-          sd.setMobile(mobile);
-          
-          subject = m.getSubject();
-          mark.setSubject(subject);
-          
-          marks = m.getMarks();
-          mark.setMarks(marks);
-          
-            System.out.println(" "+m.getMobile());
+
+        StudentDetails sd = new StudentDetails();
+        Marks mark = new Marks();
+        ClassDetails cd = new ClassDetails();
+        Student s = new Student();
+
+        for (Mixture m : list) {
+            stud_id = m.getStud_id();
+            sd.setStud_id(stud_id);
+            System.out.println(sd.getClass() + ":      stud_id:   " + sd.getStud_id());
+
+            mark.setStud_id(stud_id);
+            cd.setStud_id(stud_id);
+            System.out.println(cd.getClass() + ":      stud_id:   " + cd.getStud_id());
+
+            stud_name = m.getStud_name();
+            s.setName(stud_name);
+            System.out.println(s.getClass() + ":       stud_name:   " + s.getName());
+
+            mobile = m.getMobile();
+            sd.setMobile(mobile);
+            System.out.println(sd.getClass() + ":      stud_mobile:   " + sd.getMobile());
+
+            subject = m.getSubject();
+            mark.setSubject(subject);
+            System.out.println(mark.getClass() + ":     studsub:    " + mark.getSubject());
+
+            marks = m.getMarks();
+            mark.setMarks(marks);
+            System.out.println(mark.getClass() + ":     stud_marks:    " + mark.getMarks());
+            System.out.println("");
+
         }
     }
 }
