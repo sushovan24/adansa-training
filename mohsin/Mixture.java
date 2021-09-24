b1de477a20544416a7b4b826635129d16dc3a062
Er.Md Mohsin Alam <90970098+mohsin3456@users.noreply.github.com>
2021-09-24 15:53:46 +0530

    Add files via upload

---
+++ mohsin/Mixture.java
@@ -0,0 +1,59 @@
+
+package sept23.mohsin;
+public class Mixture {
+//Class Mixture[stud_id<int>, stud_name<String>, mobile<String>, subject<String>, marks<int>]
+    private int stud_id;
+    private String stud_name;
+    private String mobile;
+    private String subject;
+    private int marks;
+
+    public int getStud_id() {
+        return stud_id;
+    }
+
+    public void setStud_id(int stud_id) {
+        this.stud_id = stud_id;
+    }
+
+    public String getStud_name() {
+        return stud_name;
+    }
+
+    public void setStud_name(String stud_name) {
+        this.stud_name = stud_name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Mixture{" + "stud_id=" + stud_id + ", stud_name=" + stud_name + ", mobile=" + mobile + ", subject=" + subject + ", marks=" + marks + '}';
+    }
+    
+    
+}
+    
+
