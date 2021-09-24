90518e410c199d55d404b9b1404358a9ec5cc5e2
Rajkumarpandit436 <rajkumarpandit436@gmail.com>
2021-09-24 17:43:31 +0530

    changes commit

--- raj-training/src/main/java/com/practice/rajtraining/datetime/DateTimeExample.java
+++ raj-training/src/main/java/com/practice/rajtraining/datetime/DateTimeExample.java
@@ -1,29 +1,29 @@
-
 package com.practice.rajtraining.datetime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class DateTimeExample {
-    
-    public void chechDateValid(String givenDate) throws ParseException{
+
+    public void chechDateValid(String givenDate) throws ParseException {
+
         String fromDate = "20/05/2012";
         String toDate = "20/09/2021";
-        
-        
-        SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Date tdate = sdf.parse(toDate);
         Date fdate = sdf.parse(fromDate);
         Date gdate = sdf.parse(givenDate);
-        
-        boolean bool1 = tdate.after(gdate);
-        boolean bool2 = fdate.before(gdate);
-        
-        if(bool1==bool2){
+
+        boolean bool1 = fdate.before(gdate);
+        boolean bool2 = tdate.after(gdate);
+
+        System.out.println("fdate.before(gdate): "+bool1);
+        System.out.println("tdate.after(gdate): "+bool2);
+        if (bool1 == bool2) {
             System.out.println("given date is true");
-        }else{
+        } else {
             System.out.println("false");
         }
     }
