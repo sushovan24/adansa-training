929f56ea432e6759b8fcddb2939e1cc34fb33de9
Mohsin <m.mohsin0395@gmail.com>
2021-09-29 11:24:12 +0530

    mohsin collection

---
+++ collection/Marks.java
@@ -0,0 +1,53 @@
+
+package com.practice.mohsintraining.collection;
+public class Marks {
+  private int stud_id;
+  private int id;
+  private String subject;
+
+    public int getStud_id() {
+        return stud_id;
+    }
+
+    public void setStud_id(int stud_id) {
+        this.stud_id = stud_id;
+    }
+
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+  private int marks;
+  private String remarks;  
+}
+
+// logic writing 
