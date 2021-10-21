c82b66384da2e6db5b640f82eaeb9ec314ecaead
Raj Kumar Pandit <rajkumarpandit436@gmail.com>
2021-10-21 13:51:30 +0530

    m

--- mohsin-trainingEjb/mohsin-trainingEjb-ejb/src/java/com/ejb/bl/TestRemoteRemote.java
+++
@@ -1,8 +0,0 @@
-
-package com.ejb.bl;
-import javax.ejb.Remote;
-@Remote
-public interface TestRemoteRemote {// interface  class banaya hai aur uske ander method hidding kiya gya hai 
-    void printMessage(String msg);
-    void add(int a, int b);
-}
