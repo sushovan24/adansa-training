8d141cf1ba2a3f6ba5706343876c61f015b05365
mohsin <m.mohsin0395@gmail.com>
2021-10-22 23:34:09 +0530

    commit

--- raj-trainingEjb/raj-trainingEjb-ejb/src/java/com/trans/mand/bl/MandRemoteRemote.java
+++
@@ -1,9 +0,0 @@
-package com.trans.mand.bl;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface MandRemoteRemote {
-
-    void print(String msg);
-}
--- raj-trainingEjb/raj-trainingEjb-ejb/src/java/com/trans/mand/da/MandLocalLocal.java
+++
@@ -1,9 +0,0 @@
-package com.trans.mand.da;
-
-import javax.ejb.Local;
-
-@Local
-public interface MandLocalLocal {
-
-    void print(String msg);
-}
